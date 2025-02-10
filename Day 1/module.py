c234159a70ed5d6155e5a794718a2095e1216ca4
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2025-02-10 18:28:35 +0500

    Add files via upload

---
+++ Day 1/module.py
@@ -0,0 +1,7 @@
+import pyjokes
+
+print("Printing a joke")
+
+# This print a random joke
+joke = pyjokes.get_joke()
+print(joke)
---
+++ Day 1/practice1.py
@@ -0,0 +1,33 @@
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say('''Rain, rain, go away.🌧
+Come again another day.
+Daddy wants to play.
+Rain, rain, go away.
+
+Rain, rain, go away.☔
+Come again another day.
+Mommy wants to play.
+Rain, rain, go away.
+
+Rain, rain, go away.⛈
+Come again another day.
+Brother wants to play.
+Rain, rain, go away.
+
+Rain, rain, go away.🌦
+Come again another day.
+Sister wants to play.
+Rain, rain, go away.
+
+Rain, rain, go away.
+Come again another day.
+Baby wants to play.
+Rain, rain, go away.
+
+Rain, rain, go away.
+Come again another day.
+All the family wants to play.
+Rain, rain, go away.''')
+engine.runAndWait()
