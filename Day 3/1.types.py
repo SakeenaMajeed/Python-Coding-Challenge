5ed0bddfaa8f3645e4fda29846ab5e432896da44
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2025-02-22 02:08:34 +0500

    Add files via upload

---
+++ Day 3/1.types.py
@@ -0,0 +1,14 @@
+
+# type hinting
+
+name :str = "sakeena"
+print(name)
+print(type(name))
+
+age :int = 20
+print(age)      
+print(type(age))
+
+age :int = "sakeena"
+print(age)
+print(type(age))
---
+++ Day 3/2.input.py
@@ -0,0 +1,9 @@
+age : int = input("Enter your age:")
+print(age)
+print(type(age))
+
+
+
+isage :int =  int(input("Enter Your Age :"))
+print(isage)
+print(type(isage))
