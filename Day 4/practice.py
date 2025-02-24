9c0553270523be9290bffd477efdebcf1955d48d
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2025-02-24 09:16:17 +0500

    Day 4

---
+++ Day 4/practice.py
@@ -0,0 +1,38 @@
+fruit = []
+
+f1 = input("Enter Your Fruit here! :")
+fruit.append(f1)
+f2 = input("Enter Your Fruit here! :")
+fruit.append(f2)
+f3 = input("Enter Your Fruit here! :")
+fruit.append(f3)
+f4 = input("Enter Your Fruit here! :")
+fruit.append(f4)
+f5 = input("Enter Your Fruit here! :")
+fruit.append(f5)
+print(fruit)
+
+
+Marks = []
+
+s1 = int(input("Enter Your Marks here! :"))
+Marks.append(s1)
+s2 = int(input("Enter Your Marks here! :"))
+Marks.append(s2)
+s3 = int(input("Enter Your Marks here! :"))
+Marks.append(s3)
+s4 = int(input("Enter Your Marks here! :"))
+Marks.append(s4)
+s5 = int(input("Enter Your Marks here! :"))
+Marks.append(s5)
+Marks.sort()
+print(Marks) 
+
+
+# l = [22, 33, 78, 45]
+# print(sum(l))
+
+
+
+my_count = (7, 1, 2, 3, 4, 6, 7)
+print(my_count.count(7))
